9849581652a6eb5c112a0d2f8e49420f258cf870
Jaidenver <jaiderhurtado271@gmail.com>
2024-09-21 18:04:55 -0500

    Add changes for my application

---
+++ src/main/java/com/jaicol/shop/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.jaicol.shop.controller;
+
+import com.jaicol.shop.model.Product;
+import com.jaicol.shop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.findAll();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.save(product);
+    }
+
+    // Otros endpoints según tus necesidades
+}
---
+++ src/main/java/com/jaicol/shop/service/ProductService.java
@@ -0,0 +1,24 @@
+package com.jaicol.shop.service;
+
+import com.jaicol.shop.model.Product;
+import com.jaicol.shop.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    // Otros métodos según tus necesidades
+}
